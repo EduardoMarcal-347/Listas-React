927fc7cc9d77e8ee11ba4c499df12882c2c7883e
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-14 18:48:55 -0300

    inserindo dados ana bruck

--- lista05/src/App.jsx
+++ lista05/src/App.jsx
@@ -3,10 +3,10 @@ import AvaliacaoCliente from "./components/main/section/AvaliacaoCliente";
 export default function App() {
   return (
     <AvaliacaoCliente
-      nome = "Ana Brooks"
+      nome = "Ana Bruck"
       img = "foto01.jpg"
-      avaliacao01 = "legal"
-      avaliacao02 = "ok"/>
+      avaliacao01 = '"Eu recomendo a leticia, pois o atendimento dela fez toda a diferença"'
+      avaliacao02 = '"A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos."'/>
   )
 }
 
