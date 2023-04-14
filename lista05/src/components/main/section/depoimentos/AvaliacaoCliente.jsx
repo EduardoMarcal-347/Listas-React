0153ebf5478d2adcccae490f64a6fe602d658bfe
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-14 19:12:27 -0300

    iniciando css do projeto

--- lista05/src/components/main/section/depoimentos/AvaliacaoCliente.jsx
+++ lista05/src/components/main/section/depoimentos/AvaliacaoCliente.jsx
@@ -1,8 +1,8 @@
 export default function AvaliacaoCliente(props){
     return(
-        <div>
-            <h2>{props.nome}</h2>
+        <div className="avaliacaoClientes">
             <img src= {`./img/${props.img}`} alt="" />
+            <h2>{props.nome}</h2>
             <p>{props.avaliacao01}</p>
             <p>{props.avaliacao02}</p>
         </div>
