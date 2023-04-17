3f57ed59343aeb5c4d672bc93db71da68cc0a22c
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-17 18:38:20 -0300

    inicializando lista 07

---
+++ lista07/src/components/main/section/depoimentos/Depoimentos.jsx
@@ -0,0 +1,24 @@
+import AvaliacaoCliente from "./AvaliacaoCliente";
+
+export default function Depoimentos() {
+    
+    return (
+        <section className="depoimentos">
+            <AvaliacaoCliente
+                img = "foto01.jpg"
+                nome = "Ana Bruck"
+                avaliacao01 = '"Eu recomendo a leticia, pois o atendimento dela fez toda a diferença"'
+                avaliacao02 = '"A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos."'/>
+            <AvaliacaoCliente
+                img = "foto02.jpg"
+                nome = "Dani Schutz"
+                avaliacao01 = '"Eu recomendo a leticia, pois o atendimento dela fez toda a diferença"'
+                avaliacao02 = '"A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos."'/>
+            <AvaliacaoCliente
+                img = "foto03.jpg"
+                nome = "Bia Wolf"
+                avaliacao01 = '"Eu recomendo a leticia, pois o atendimento dela fez toda a diferença"'
+                avaliacao02 = '"A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos."'/>
+        </section>
+    )
+}
