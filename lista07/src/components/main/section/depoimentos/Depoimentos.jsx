fe038e61a8f73a3586c36101268eeaeebc6ff429
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-18 18:58:47 -0300

    consumindo json avaliacoes

--- lista07/src/components/main/section/depoimentos/Depoimentos.jsx
+++ lista07/src/components/main/section/depoimentos/Depoimentos.jsx
@@ -1,11 +1,24 @@
 import AvaliacaoCliente from "./AvaliacaoCliente";
 
 export default function Depoimentos() {
+    
+    function getJsonAvaliacoes(){
+        let filename = './json/avaliacaoClientes.json'
+        let vetAvaliacao = []
+        fetch(filename)
+        .then(response => response.json())
+        .then(data => Object.values(data).forEach(e => vetAvaliacao.push(e)))
+        .catch(error => console.error(error))
+        return vetAvaliacao;
+    }
+
+    let avaliacaoClientes = getJsonAvaliacoes();
+    console.log(avaliacaoClientes);
 
     return (
         <section className="depoimentos">
             <AvaliacaoCliente
-                img = "foto01.jpg"
+                img = "foto02.jpg"
                 nome = "Ana Bruck"
                 avaliacao01 = '"Eu recomendo a leticia, pois o atendimento dela fez toda a diferença"'
                 avaliacao02 = '"A escolha de um imóvel, no nosso caso uma casa, não é uma tarefa fácil. A escolha de um profissional qualificado poupou o nosso tempo e nos ajudou a encontrar o que realmente queriamos."'/>
